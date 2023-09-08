e33e4adab4ae0fb487b6ab1ecaec6bc3694820d0
sebas <sebas@LAPTOP-8VT65DQL>
2023-09-08 08:52:14 -0500

    Updated

---
+++ src/main/java/com/mycompany/firebase/Firebase.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.firebase;
+
+
+/**
+ *
+ * @author sebas
+ */
+public class Firebase {
+
+        public static void main(String[] args) {
+        ConexionFirebase.conectarFirebase();
+    }
+}
